a27b81140e05b8ef21e418b265cc676312045211
Anvith Vegi <anvith.vegi@gmail.com>
2025-11-04 08:59:51 +0530

    Binary Search Tree FIles

---
+++ bst.h
@@ -0,0 +1,12 @@
+#ifndef BST_H
+#define BST_H
+#include "change_request.h"
+typedef struct Node {
+    ChangeRequest data;
+    struct Node *left, *right;
+} Node;
+Node* insertBST(Node* root, ChangeRequest data);
+Node* searchBST(Node* root, int id);
+Node* deleteBST(Node* root, int id);
+void inorderBST(Node* root);
+#endif
