eb72765ab2b4cde332c4e2e0ba24d425941c1a38
Anvith Vegi <anvith.vegi@gmail.com>
2025-11-04 14:42:21 +0530

    Formatted BST Functions

--- bst.c
+++ bst.c
@@ -2,9 +2,71 @@
 #include <stdlib.h>
 #include <string.h>
 #include "bst.h"
-Node* createNode(ChangeRequest data){Node* newNode=(Node*)malloc(sizeof(Node));newNode->data=data;newNode->left=newNode->right=NULL;return newNode;}
-Node* insertBST(Node* root, ChangeRequest data){if(!root)return createNode(data);if(data.id<root->data.id)root->left=insertBST(root->left,data);else if(data.id>root->data.id)root->right=insertBST(root->right,data);return root;}
-Node* searchBST(Node* root,int id){if(!root||root->data.id==id)return root;if(id<root->data.id)return searchBST(root->left,id);return searchBST(root->right,id);}
-Node* minValueNode(Node* n){Node* c=n;while(c&&c->left)c=c->left;return c;}
-Node* deleteBST(Node* root,int id){if(!root)return root;if(id<root->data.id)root->left=deleteBST(root->left,id);else if(id>root->data.id)root->right=deleteBST(root->right,id);else{if(!root->left){Node*t=root->right;free(root);return t;}else if(!root->right){Node*t=root->left;free(root);return t;}Node*t=minValueNode(root->right);root->data=t->data;root->right=deleteBST(root->right,t->data.id);}return root;}
-void inorderBST(Node* root){if(!root)return;inorderBST(root->left);printf("ID:%d | Title:%s | Priority:%d | Status:%s\n",root->data.id,root->data.title,root->data.priority,root->data.status);inorderBST(root->right);}
+Node *createNode(ChangeRequest data)
+{
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+Node *insertBST(Node *root, ChangeRequest data)
+{
+    if (!root)
+        return createNode(data);
+    if (data.id < root->data.id)
+        root->left = insertBST(root->left, data);
+    else if (data.id > root->data.id)
+        root->right = insertBST(root->right, data);
+    return root;
+}
+Node *searchBST(Node *root, int id)
+{
+    if (!root || root->data.id == id)
+        return root;
+    if (id < root->data.id)
+        return searchBST(root->left, id);
+    return searchBST(root->right, id);
+}
+Node *minValueNode(Node *n)
+{
+    Node *c = n;
+    while (c && c->left)
+        c = c->left;
+    return c;
+}
+Node *deleteBST(Node *root, int id)
+{
+    if (!root)
+        return root;
+    if (id < root->data.id)
+        root->left = deleteBST(root->left, id);
+    else if (id > root->data.id)
+        root->right = deleteBST(root->right, id);
+    else
+    {
+        if (!root->left)
+        {
+            Node *t = root->right;
+            free(root);
+            return t;
+        }
+        else if (!root->right)
+        {
+            Node *t = root->left;
+            free(root);
+            return t;
+        }
+        Node *t = minValueNode(root->right);
+        root->data = t->data;
+        root->right = deleteBST(root->right, t->data.id);
+    }
+    return root;
+}
+void inorderBST(Node *root)
+{
+    if (!root)
+        return;
+    inorderBST(root->left);
+    printf("ID:%d | Title:%s | Priority:%d | Status:%s\n", root->data.id, root->data.title, root->data.priority, root->data.status);
+    inorderBST(root->right);
+}
