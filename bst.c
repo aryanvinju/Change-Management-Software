bf49e2c430bf8da8d0d302187b50755746d17585
Anvith Vegi <anvith.vegi@gmail.com>
2025-11-19 11:40:11 +0530

    Implement additional BST operations
    Added functions to update priorities, count nodes, and convert BST to array.

--- bst.c
+++ bst.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "bst.h"
+
 Node *createNode(ChangeRequest data)
 {
     Node *newNode = (Node *)malloc(sizeof(Node));
@@ -9,16 +10,24 @@ Node *createNode(ChangeRequest data)
     newNode->left = newNode->right = NULL;
     return newNode;
 }
+
 Node *insertBST(Node *root, ChangeRequest data)
 {
     if (!root)
         return createNode(data);
+    
+    if (data.id == root->data.id) {
+        return root; // Should be prevented by main.c
+    }
+    
     if (data.id < root->data.id)
         root->left = insertBST(root->left, data);
     else if (data.id > root->data.id)
         root->right = insertBST(root->right, data);
+    
     return root;
 }
+
 Node *searchBST(Node *root, int id)
 {
     if (!root || root->data.id == id)
@@ -27,6 +36,7 @@ Node *searchBST(Node *root, int id)
         return searchBST(root->left, id);
     return searchBST(root->right, id);
 }
+
 Node *minValueNode(Node *n)
 {
     Node *c = n;
@@ -34,6 +44,7 @@ Node *minValueNode(Node *n)
         c = c->left;
     return c;
 }
+
 Node *deleteBST(Node *root, int id)
 {
     if (!root)
@@ -62,6 +73,7 @@ Node *deleteBST(Node *root, int id)
     }
     return root;
 }
+
 void inorderBST(Node *root)
 {
     if (!root)
@@ -69,4 +81,37 @@ void inorderBST(Node *root)
     inorderBST(root->left);
     printf("ID:%d | Title:%s | Priority:%d | Status:%s\n", root->data.id, root->data.title, root->data.priority, root->data.status);
     inorderBST(root->right);
+}
+
+void updatePrioritiesBST(Node* root, int newPriority) {
+    if (!root) 
+        return;
+    
+    if (root->data.priority >= newPriority) {
+        root->data.priority++;
+    }
+    
+    updatePrioritiesBST(root->left, newPriority);
+    updatePrioritiesBST(root->right, newPriority);
+}
+
+int countNodes(Node* root) {
+    if (root == NULL) {
+        return 0;
+    }
+    return 1 + countNodes(root->left) + countNodes(root->right);
+}
+
+void bstToArray(Node* root, ChangeRequest* arr, int* index) {
+    if (root == NULL) {
+        return;
+    }
+    
+    bstToArray(root->left, arr, index);
+    
+    arr[*index] = root->data;
+    (*index)++;
+    
+
+    bstToArray(root->right, arr, index);
 }
