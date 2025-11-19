66c9af34c11d939e0372ebaf3dc6f6eab7b0392c
Anvith Vegi <anvith.vegi@gmail.com>
2025-11-19 11:40:36 +0530

    Add new BST utility functions
    Added functions to update priorities, count nodes, and convert BST to array.

--- bst.h
+++ bst.h
@@ -1,12 +1,19 @@
 #ifndef BST_H
 #define BST_H
 #include "change_request.h"
+
 typedef struct Node {
     ChangeRequest data;
     struct Node *left, *right;
 } Node;
+
 Node* insertBST(Node* root, ChangeRequest data);
 Node* searchBST(Node* root, int id);
 Node* deleteBST(Node* root, int id);
 void inorderBST(Node* root);
+void updatePrioritiesBST(Node* root, int newPriority);
+int countNodes(Node* root);
+void bstToArray(Node* root, ChangeRequest* arr, int* index);
+
+
 #endif
