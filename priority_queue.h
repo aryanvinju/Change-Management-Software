73513b298fec1477aa2d0831a367ef9505e17322
Anvith Vegi <anvith.vegi@gmail.com>
2025-11-19 11:41:40 +0530

    Modify PriorityQueue to support dynamic capacity
    Refactor PriorityQueue structure to use dynamic memory allocation and update function signatures.

--- priority_queue.h
+++ priority_queue.h
@@ -1,13 +1,20 @@
 #ifndef PRIORITY_QUEUE_H
 #define PRIORITY_QUEUE_H
 #include "change_request.h"
-typedef struct{
-    ChangeRequest heap[100];
-    int size;
-}PriorityQueue;
 
-void initPQ(PriorityQueue* pq);
-void enqueue(PriorityQueue* pq, ChangeRequest data);
-ChangeRequest dequeue(PriorityQueue* pq);
-int isPQEmpty(PriorityQueue* pq);
+typedef struct
+{
+    ChangeRequest *heap;
+    int size;
+    int capacity;
+} PriorityQueue;
+
+void initPQ(PriorityQueue *pq, int capacity);
+void enqueue(PriorityQueue *pq, ChangeRequest data);
+ChangeRequest dequeue(PriorityQueue *pq);
+int isPQEmpty(PriorityQueue *pq);
+void heapifyDown(PriorityQueue *pq, int i);
+void updatePrioritiesPQ(PriorityQueue *pq, int newPriority);
+void removeByIdFromPQ(PriorityQueue *pq, int id);
+
 #endif
